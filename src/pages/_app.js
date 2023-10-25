8b925c2f64d5b1d750f0d920ef110e234aaae975
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-10-25 18:12:12 -0400

    Tailwind setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@bretzkierra/styles/globals.css'
+import '@bretzkierra/styles/globals.css';
+import './globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
