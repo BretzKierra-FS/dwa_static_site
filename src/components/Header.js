d15260e5b73c4ea28a55f7b2b25e94ebe138ec72
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-10-29 11:53:58 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="m-auto">
-      <img src="public/tent.SVG" className="w-full absolute" />
+      <img src="/public/tent.SVG" className="w-full absolute" />
       <img
-        src="/logo.png"
+        src="/public/logo.png"
         className="m-auto relative pt-20
       "
       />
